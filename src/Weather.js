8df49ad570c49c4cf85acc922616417faf5515a4
maboelion <mareike.boettcher@gmail.com>
2021-08-30 16:35:31 +0200

    CSS updated / included

--- src/Weather.js
+++ src/Weather.js
@@ -9,45 +9,62 @@ export default function Weather() {
 			<form>
 				<div className="row">
 					<div className="col-6">
-						<input type="search" placeholder="Enter a city..." autofocus="on" />
+						<input
+							type="search"
+							placeholder="Enter a city..."
+							autofocus="on"
+							className="form-control w-100"
+						/>
 					</div>
 					<div className="col-3">
-						<input className="btn btn-dark" type="submit" value="Search" />
+						<input
+							className="btn btn-dark w-100"
+							type="submit"
+							value="Search"
+						/>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-dark">Current</button>
+						<button className="btn btn-dark w-100">Current</button>
 					</div>
 				</div>
 			</form>
-			<div className="row">
-				<div className="col-4">
-					<h2>New York</h2>
-					<ul>
-						<li>Monday 13:47</li>
-						<li>Cloudy</li>
-						<li>
-							<img
-								src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-								alt="Cloudy"
-							/>
-						</li>
-					</ul>
-				</div>
-				<div className="col-4">
-					<ul>
-						<li>
-							<h3>20°</h3>
-						</li>
-						<li>°C | °F</li>
-					</ul>
-				</div>
-				<div className="col-4">
-					<ul>
-						<li>Sunrise Time: 08:00</li>
-						<li>Sunset Time: 20:00</li>
-						<li>Humidity: 80%</li>
-						<li>Wind: 13km/h</li>
-					</ul>
+			<div className="weather-display">
+				<div className="row">
+					<div className="col-4">
+						<h2>New York</h2>
+						<ul>
+							<li>Monday 13:47</li>
+							<li className="mt-3 ms-4">
+								<img
+									src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+									alt="Cloudy"
+								/>
+								<br />
+								<small className="ms-2">Cloudy</small>
+							</li>
+						</ul>
+					</div>
+					<div className="col-4 text-center">
+						<ul>
+							<li>
+								<h3>20°</h3>
+							</li>
+							<li>
+								19°C | <strong>25°C</strong>
+							</li>
+							<li className="mt-4">
+								<button className="btn btn-light">Fahrenheit</button>
+							</li>
+						</ul>
+					</div>
+					<div className="col-4 text-end mt-4">
+						<ul>
+							<li>Sunrise Time: 08:00</li>
+							<li>Sunset Time: 20:00</li>
+							<li className="mt-4">Humidity: 80%</li>
+							<li>Wind: 13km/h</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
